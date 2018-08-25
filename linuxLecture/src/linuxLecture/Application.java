bee4dd9dc4e7cdf95e5eac3cc00ba7d89ae4afe2
eliko <yyyeliko@gmail.com>
2018-08-25 18:06:18 +0300

    change to 15

--- linuxLecture/src/linuxLecture/Application.java
+++ linuxLecture/src/linuxLecture/Application.java
@@ -3,9 +3,9 @@ package linuxLecture;
 public class Application {
 
 	public static void main(String[] args) {
-		//Create 'for loop' that count to 5
+		//Create 'for loop' that count to 15
 		
-		int number = 5;
+		int number = 15;
 		
 		for (int i = 0; i < number; i++) {
 			System.out.println(i+1);
