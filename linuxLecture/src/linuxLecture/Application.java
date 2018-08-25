901a0d02fce0f4fe71af321f2e57deee5610da35
eliko <yyyeliko@gmail.com>
2018-08-25 17:57:56 +0300

    Original project

---
+++ linuxLecture/src/linuxLecture/Application.java
@@ -0,0 +1,17 @@
+package linuxLecture;
+
+public class Application {
+
+	public static void main(String[] args) {
+		//Create 'for loop' that count to 5
+		
+		int letter = 10;
+		
+		for (int i = 0; i < letter; i++) {
+			System.out.println(i+1);
+		}
+		
+		
+	}
+
+}
