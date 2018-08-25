25888185565a4c58059302b22248d42ff8dd393c
benhalfon1994 <benhalfon1994@gmail.com>
2018-08-25 18:03:18 +0300

    fix loop to stop at 5 fix #1

--- linuxLecture/src/linuxLecture/Application.java
+++ linuxLecture/src/linuxLecture/Application.java
@@ -5,9 +5,9 @@ public class Application {
 	public static void main(String[] args) {
 		//Create 'for loop' that count to 5
 		
-		int letter = 10;
+		int number = 5;
 		
-		for (int i = 0; i < letter; i++) {
+		for (int i = 0; i < number; i++) {
 			System.out.println(i+1);
 		}
 		
