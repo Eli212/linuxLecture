1a4045983b19ff739eb35d0e282e3b9c4e4c19e2
benhalfon1994 <benhalfon1994@gmail.com>
2018-08-25 18:05:48 +0300

    change to 10

--- linuxLecture/src/linuxLecture/Application.java
+++ linuxLecture/src/linuxLecture/Application.java
@@ -3,9 +3,9 @@ package linuxLecture;
 public class Application {
 
 	public static void main(String[] args) {
-		//Create 'for loop' that count to 5
+		//Create 'for loop' that count to 10
 		
-		int number = 5;
+		int number = 10;
 		
 		for (int i = 0; i < number; i++) {
 			System.out.println(i+1);
