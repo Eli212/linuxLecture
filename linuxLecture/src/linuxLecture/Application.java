546376a9c04365653b34770bffa41ee080944406
eliko <yyyeliko@gmail.com>
2018-08-31 09:06:03 +0300

    code by eli

--- linuxLecture/src/linuxLecture/Application.java
+++ linuxLecture/src/linuxLecture/Application.java
@@ -3,8 +3,7 @@ package linuxLecture;
 public class Application {
 
 	public static void main(String[] args) {
-		//Create 'for loop' that count to 15
-		
+		//Create 'for loop' that count to 10
 		int number = 15;
 		
 		for (int i = 0; i < number; i++) {
