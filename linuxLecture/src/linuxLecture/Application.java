55eb56cf5695d3d110a108c8ef840665bd04e142
benhalfon <benhalfon1994@gmail.com>
2018-08-31 10:26:38 +0300

    fix the counting to 10 fix #3

--- linuxLecture/src/linuxLecture/Application.java
+++ linuxLecture/src/linuxLecture/Application.java
@@ -4,9 +4,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		//Create 'for loop' that count to 10
-		int number = 15;
-		
-		//
+		int number = 10;
 		
 		for (int i = 0; i < number; i++) {
 			System.out.println(i+1);
